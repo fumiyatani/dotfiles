4d5aba788295c9cbd812c742a1703b2891c6b743
tanifumiya <fumiyadeveloper@gmail.com>
2026-01-07 01:02:38 +0900

    zsh-completions と zsh-syntax-highlighting を追加

--- zsh/.zsh/brew-plugins.zsh
+++ zsh/.zsh/brew-plugins.zsh
@@ -1,2 +1,8 @@
+# zsh-completions
+FPATH="/opt/homebrew/share/zsh-completions:$FPATH"
+
 # zsh-autosuggestions (Homebrew)
 source "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+# zsh-syntax-highlighting
+source "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
