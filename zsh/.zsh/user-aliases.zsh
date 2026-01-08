1964d813fc5b3b63be3ccaffed777790e7924a6f
tanifumiya <fumiyadeveloper@gmail.com>
2026-01-08 23:02:06 +0900

    エイリアスをuser-aliases.zshに分離
    - zsh/.zsh/user-aliases.zsh を新規作成
    - .zshrc からエイリアス定義を削除し、user-aliases.zsh を読み込むように変更
    - Git, Docker, PostgreSQL, Firebase, FVM/Flutter のエイリアスを移動

---
+++ zsh/.zsh/user-aliases.zsh
@@ -0,0 +1,24 @@
+################################
+# User-defined Aliases
+################################
+# Git
+alias g='git'
+
+# Docker
+alias d='docker'
+alias dc='docker compose'
+
+# PostgreSQL (for Homebrew)
+alias psqlstart='brew services start postgresql'
+alias psqlstop='brew services stop postgresql'
+
+# Firebase (Android) – debug log
+alias firelog='adb shell setprop log.tag.FA VERBOSE && \
+               adb shell setprop log.tag.FA-SVC VERBOSE && \
+               adb logcat -v time -s FA FA-SVC'
+
+# FVM / Flutter
+alias f='fvm'
+alias ff='fvm flutter'
+alias fd='fvm dart'
+alias fdb='fvm dart run build_runner build -d'
